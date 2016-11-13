699fcea6826df6fac0f0851ac1a6c437d86be6d6
micky387 <mickaelsaibi@free.fr>
2016-11-13 22:40:02 +0100

    setup: fix wifi for G variant

--- arch/arm/kernel/setup.c
+++ arch/arm/kernel/setup.c
@@ -644,6 +644,15 @@ static int __init msm_hw_rev_setup(char *p)
 }
 early_param("androidboot.revision", msm_hw_rev_setup);
 
+static int __init msm_hw_rev_setup_G(char *p)
+{
+	system_rev = memparse(p, NULL);
+	printk("androidboot.revision %x\n", system_rev);	
+	return 0;
+}
+
+early_param("androidboot.revision", msm_hw_rev_setup_G);
+
 static void __init request_standard_resources(struct machine_desc *mdesc)
 {
 	struct memblock_region *region;
